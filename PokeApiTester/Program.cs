da2156e8d2983bf567109705729ca4eaa1c8c0d9
CalvinSithideth <calvin.sithideth@gmail.com>
2019-11-24 03:11:32 -0800

    Add project files.

---
+++ PokeApiTester/Program.cs
@@ -0,0 +1,27 @@
+﻿using PokemonAppAPI;
+using System;
+using System.Threading.Tasks;
+
+namespace PokeApiTester
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            PokemonClient client = new PokemonClient();
+            PokemonResponse result = await client.GetPokemon(1);
+
+            Console.WriteLine($"Name: {result.name}");
+            Console.WriteLine($"Weight: {result.weight}");
+
+            Console.WriteLine("\n\n" + "Abilities" + "\n");
+            foreach (var ability in result.abilities)
+            {
+                Console.WriteLine(ability.ability.name);
+                Console.WriteLine($"More information at: {ability.ability.url}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PokemonAppAPI/PokemonClient.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PokemonAppAPI
+{
+    public class PokemonClient
+    {
+        private static readonly HttpClient _client;
+
+        static PokemonClient()
+        {
+            _client = new HttpClient();
+            // Must end with a forward slash
+            _client.BaseAddress = new Uri("https://pokeapi.co/api/v2/");
+        }
+
+        /// <summary>
+        /// Returns a single Pokemon's data using Id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public async Task<PokemonResponse> GetPokemon(int id)
+        {
+            return await GetPokemon(id.ToString());
+        }
+
+        /// <summary>
+        /// Returns a single Pokemons data using name.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public async Task<PokemonResponse> GetPokemon(string name)
+        {
+            HttpResponseMessage response = await _client.GetAsync($"pokemon/{name}");
+            if (response.IsSuccessStatusCode)
+            {
+                string data = await response.Content.ReadAsStringAsync();
+
+                return JsonConvert.DeserializeObject<PokemonResponse>(data);
+            }
+            else
+            {
+                throw new Exception("Call was not successful.");
+            }
+        }
+    }
+}

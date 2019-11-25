3cc6b847992dd45b610087e51852c9534debdacc
CalvinSithideth <calvin.sithideth@gmail.com>
2019-11-25 15:28:03 -0800

    Added static class to assist in converting API units to canon Pokemon units

---
+++ PokemonApp/Models/UnitConversionHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PokemonApp.Models
+{
+    public static class UnitConversionHelper
+    {
+
+        public static string DecimetersToFeetAndInches(int decimeters)
+        {
+            const double InchesPerDecimeter = 3.937;
+            const double InchesPerFeet = 12;
+
+            double totalInches = decimeters * InchesPerDecimeter;
+            int feet = (int) (totalInches / InchesPerFeet);
+            int inchesLeft = (int) Math.Round(totalInches % InchesPerFeet);
+            return $@"{feet}'{inchesLeft}""";
+        }
+
+        public static string HectogramsToPoundsAndOunces(int hectograms)
+        {
+            const double OuncesPerHectogram = 3.527;
+            const double OuncesPerPound = 16;
+
+            throw new NotImplementedException();
+        }
+    }
+}
